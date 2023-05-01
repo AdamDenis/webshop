82c370d157797eb6a4ad7db8975e92524a1a3e90
AdamDenis <dipper200213@gmail.com>
2023-05-01 16:13:50 +0300

    add cart-page component

---
+++ src/app/cart-page/cart-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart/cart.service';
+import {Cart} from "../shared/models/Cart";
+import {CartItem} from "../shared/models/CartItem";
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent implements OnInit {
+  cart!:Cart;
+  constructor(private cartService: CartService) {
+    this.setCart();
+  }
+
+  ngOnInit() {
+  }
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.cloth.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.cloth.id, quantity);
+    this.setCart();
+  }
+  setCart(){
+    this.cart = this.cartService.getCart();
+  }
+}
